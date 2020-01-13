fa455aad145332ea2f34bb2e341f1a911b31435b
unknown <bettinagr@hotmail.fr>
2020-01-13 11:57:45 +0100

    Upload photo test

---
+++ src/main/java/fr/uga/miashs/sempic/backingbeans/UploadPhoto.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.uga.miashs.sempic.backingbeans;
+
+import fr.uga.miashs.sempic.SempicException;
+import fr.uga.miashs.sempic.SempicModelException;
+import fr.uga.miashs.sempic.dao.AlbumFacade;
+import fr.uga.miashs.sempic.dao.PhotoFacade;
+import fr.uga.miashs.sempic.dao.PhotoStorage;
+import fr.uga.miashs.sempic.entities.SempicPhoto;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.ServletContext;
+import javax.servlet.http.Part;
+import javax.xml.bind.DatatypeConverter;
+import jdk.internal.net.http.common.Utils;
+
+/**
+ *
+ * @author Bettina
+ */
+@Named
+@ViewScoped
+public class UploadPhoto implements Serializable{
+    
+    private Part photo;
+    
+    private SempicPhoto current;
+    
+    @Inject
+    private AlbumFacade albumDao;
+    
+    @Inject
+    private PhotoStorage photoStorage;
+    
+    @Inject
+    private PhotoFacade photoDao;
+    
+    private static final Map <String, String> mimeTypes; 
+     
+    static {
+        Map <String, String> aMap = new HashMap <String, String>();
+        aMap.put("image/png", "png");
+        aMap.put("image/jpeg", "jpeg");
+        mimeTypes = Collections.unmodifiableMap(aMap);
+    }
+    
+    public UploadPhoto(){
+         try {
+            photoStorage = new PhotoStorage();
+        } catch (IOException ex) {
+            Logger.getLogger(UploadPhoto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    @PostConstruct
+    public void init() {
+        current=new SempicPhoto();
+    }
+    
+    public SempicPhoto getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(SempicPhoto current) {
+        this.current = current;
+    }
+    
+    public Part getPhoto(){
+        return photo;
+    }
+    
+    public void setPhoto(Part p){
+        this.photo = p;
+    }
+     public String save() throws Exception {
+        try  {
+            InputStream input = photo.getInputStream();
+            String mime = photo.getContentType();
+            
+            if (mimeTypes.containsKey(mime)){
+                
+                String fileName = createSha1(input) + "." + mimeTypes.get(mime);
+                input = photo.getInputStream(); // mark(0) doesnt work, so we initialize again
+            //  String fileName = "test.jpeg";
+                photoStorage.savePicture(PhotoStorage.pictureStore.resolve(fileName),input);
+                SempicPhoto p = new SempicPhoto();
+                p.setName(fileName);
+                //p.setAlbum(albumDao.getById(albumId));
+                photoDao.create(p);
+            } 
+            
+        }
+        catch (IOException e) {
+            System.out.println("erreur upload");
+        
+            return "failure";
+        }
+        return "success";
+    }
+     private String createSha1(InputStream fis) throws IOException, NoSuchAlgorithmException  {
+        MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("SHA-1");
+             int n = 0;
+            byte[] buffer = new byte[8192];
+            while (n != -1) {
+                n = fis.read(buffer);
+                if (n > 0) {
+                    digest.update(buffer, 0, n);
+                }
+            }
+            return  DatatypeConverter.printHexBinary(digest.digest());
+
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(UploadPhoto.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        return null;
+    }
+}
---
+++ src/main/java/fr/uga/miashs/sempic/entities/SempicPhoto.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.uga.miashs.sempic.entities;
+
+import fr.uga.miashs.restservice.PhotoStore;
+import fr.uga.miashs.sempic.SempicException;
+import fr.uga.miashs.sempic.dao.PhotoStorage;
+import java.io.IOException;
+import java.io.Serializable;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.servlet.http.Part;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author Bettina
+ */
+@Entity
+@Table
+public class SempicPhoto implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    
+    private String name;
+    
+    @NotNull
+    @ManyToOne
+    private SempicAlbum album;
+    
+    public SempicPhoto(){
+        
+    }
+    
+     public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public SempicAlbum getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(SempicAlbum album) {
+        this.album = album;
+    }
+    
+    public String getPicturePath() {
+        return PhotoStorage.pictureStore.resolve(name).toString();
+    }
+   
+    public String getThumbnailPath() throws SempicException {
+        Path pic = Paths.get(name);
+        try {
+            PhotoStorage ps = new PhotoStorage();
+            ps.getThumbnailPath(pic, 120);
+            return new PhotoStorage(PhotoStorage.thumbnailStore.resolve(String.valueOf(120)), pic).toString();
+        } catch (IOException ex) {
+            Logger.getLogger(SempicPhoto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "";
+    }
+    
+    @Override
+    public String toString() {
+        return "Photo" + "id=" + id + ", name=" + name +'}';
+    }
+}
